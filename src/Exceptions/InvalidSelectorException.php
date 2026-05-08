7f60977a2a3b4d30ad038aadf294473ca47066a2
aha <mike@magic.org>
2026-05-08 16:44:48 +0800

    first commit

---
+++ src/Exceptions/InvalidSelectorException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace zxf\Dom\Exceptions;
+
+use RuntimeException;
+
+/**
+ * 无效选择器异常类
+ * 
+ * 当提供的选择器表达式无效或无法解析时抛出
+ * 
+ * @package zxf\Dom\Exceptions
+ */
+class InvalidSelectorException extends RuntimeException
+{
+}
